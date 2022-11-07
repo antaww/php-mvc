dd6e9d1baa5edebd266d6b7e4c92bfdf9adca8eb
Bahsiik <o.mist1303@gmail.com>
2022-11-07 12:12:29 +0100

    feat: add DatabaseConnection

--- src/model/todo.php
+++ src/model/todo.php
@@ -1,1 +1,17 @@
 <?php
+
+namespace Application\Model\Todo;
+
+class Todo
+{
+    private $id;
+    private $todo;
+    private $status;
+
+    public function __construct($id, $todo, $status)
+    {
+        $this->id = $id;
+        $this->todo = $todo;
+        $this->status = $status;
+    }
+}
