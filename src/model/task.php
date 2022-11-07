e22d683bb21c80ec13d4fb71cfff34b12ef8399d
Ayfri <pierre.ayfri@gmail.com>
2022-11-07 14:39:51 +0100

    refactor: Remove constructor for Task class, reformat code.

--- src/model/task.php
+++ src/model/task.php
@@ -2,74 +2,58 @@
 
 namespace Application\Model\Todo;
 
+require_once('src/lib/DatabaseConnection.php');
+
 use Application\Lib\Database\DatabaseConnection;
 use PDO;
 
-class Task
-{
-    public int $priority;
-    public string $name;
-
-    /**
-     * @param int $priority
-     * @param string $name
-     */
-    public function __construct(int $priority, string $name)
-    {
-        $this->priority = $priority;
-        $this->name = $name;
-    }
+class Task {
+	public string $name;
+	public int $priority;
 }
 
-class TaskRepository
-{
-    public PDO $databaseConnection;
-    public function __construct() {
-	    $this->databaseConnection = (new DatabaseConnection())->getConnection();
-    }
+class TaskRepository {
+	public PDO $databaseConnection;
 
-    public function getTasks(): array
-    {
-        $statement = $this->databaseConnection->prepare('SELECT * FROM tasks');
-        $statement->execute();
-        return $statement->fetchAll(PDO::FETCH_CLASS, Task::class);
-    }
+	public function __construct() {
+		$this->databaseConnection = (new DatabaseConnection())->getConnection();
+	}
 
-    public function getTask(string $name): Task
-    {
-        $statement = $this->databaseConnection->prepare('SELECT * FROM tasks WHERE name = :name');
-        $statement->execute(['name' => $name]);
-        return $statement->fetchObject(Task::class);
-    }
+	public function addTask(string $name, int $priority): void {
+		$statement = $this->databaseConnection->prepare('INSERT INTO tasks (name, priority) VALUES (:name, :priority)');
+		$statement->execute([
+			'name' => $name,
+			'priority' => $priority,
+		]);
+	}
 
+	public function deleteAllTasks(): void {
+		$statement = $this->databaseConnection->prepare('DELETE FROM tasks');
+		$statement->execute();
+	}
 
-    public function addTask(Task $task): void
-    {
-        $statement = $this->databaseConnection->prepare('INSERT INTO tasks (name, priority) VALUES (:name, :priority)');
-        $statement->execute([
-            'name' => $task->name,
-            'priority' => $task->priority
-        ]);
-    }
+	public function deleteTask(string $name): void {
+		$statement = $this->databaseConnection->prepare('DELETE FROM tasks WHERE name = :name');
+		$statement->execute(['name' => $name]);
+	}
 
-    public function deleteTask(string $name): void
-    {
-        $statement = $this->databaseConnection->prepare('DELETE FROM tasks WHERE name = :name');
-        $statement->execute(['name' => $name]);
-    }
+	public function getTask(string $name): Task {
+		$statement = $this->databaseConnection->prepare('SELECT * FROM tasks WHERE name = :name');
+		$statement->execute(['name' => $name]);
+		return $statement->fetchObject(Task::class);
+	}
 
-    public function deleteAllTasks(): void
-    {
-        $statement = $this->databaseConnection->prepare('DELETE FROM tasks');
-        $statement->execute();
-    }
+	public function getTasks(): array {
+		$statement = $this->databaseConnection->prepare('SELECT * FROM tasks');
+		$statement->execute();
+		return $statement->fetchAll(PDO::FETCH_CLASS, Task::class);
+	}
 
-    public function updatePriority(string $name, int $priority): void
-    {
-        $statement = $this->databaseConnection->prepare('UPDATE tasks SET priority = :priority WHERE name = :name');
-        $statement->execute([
-            'name' => $name,
-            'priority' => $priority
-        ]);
-    }
+	public function updatePriority(string $name, int $priority): void {
+		$statement = $this->databaseConnection->prepare('UPDATE tasks SET priority = :priority WHERE name = :name');
+		$statement->execute([
+			'name' => $name,
+			'priority' => $priority,
+		]);
+	}
 }
