adb1924a5fe8a693ac3b726d0fa5549ec603e905
Ayfri <pierre.ayfri@gmail.com>
2022-11-07 14:46:38 +0100

    refactor: Simplify code.

--- src/model/task.php
+++ src/model/task.php
@@ -21,39 +21,30 @@ class TaskRepository {
 
 	public function addTask(string $name, int $priority): void {
 		$statement = $this->databaseConnection->prepare('INSERT INTO tasks (name, priority) VALUES (:name, :priority)');
-		$statement->execute([
-			'name' => $name,
-			'priority' => $priority,
-		]);
+		$statement->execute(compact('name', 'priority'));
 	}
 
 	public function deleteAllTasks(): void {
-		$statement = $this->databaseConnection->prepare('DELETE FROM tasks');
-		$statement->execute();
+		$this->databaseConnection->query('DELETE FROM tasks');
 	}
 
 	public function deleteTask(string $name): void {
 		$statement = $this->databaseConnection->prepare('DELETE FROM tasks WHERE name = :name');
-		$statement->execute(['name' => $name]);
+		$statement->execute(compact('name'));
 	}
 
 	public function getTask(string $name): Task {
 		$statement = $this->databaseConnection->prepare('SELECT * FROM tasks WHERE name = :name');
-		$statement->execute(['name' => $name]);
+		$statement->execute(compact('name'));
 		return $statement->fetchObject(Task::class);
 	}
 
 	public function getTasks(): array {
-		$statement = $this->databaseConnection->prepare('SELECT * FROM tasks');
-		$statement->execute();
-		return $statement->fetchAll(PDO::FETCH_CLASS, Task::class);
+		return $this->databaseConnection->query('SELECT * FROM tasks')->fetchAll(PDO::FETCH_CLASS, Task::class);
 	}
 
 	public function updatePriority(string $name, int $priority): void {
 		$statement = $this->databaseConnection->prepare('UPDATE tasks SET priority = :priority WHERE name = :name');
-		$statement->execute([
-			'name' => $name,
-			'priority' => $priority,
-		]);
+		$statement->execute(compact('name', 'priority'));
 	}
 }
