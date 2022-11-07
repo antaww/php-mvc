dd6c7fd5917f75cd90b756a3acba4c72a98c6f3e
Ayfri <pierre.ayfri@gmail.com>
2022-11-07 14:38:41 +0100

    feat: Add homepage controller.

--- src/controllers/homepage.php
+++ src/controllers/homepage.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace App\Controllers\Homepage;
+
+require_once('src/model/task.php');
+
+use Application\Model\Todo\TaskRepository;
+
+class Homepage {
+	public function execute(): void {
+		(new TaskRepository())->getTasks();
+
+		require('templates/homepage.php');
+	}
+}
