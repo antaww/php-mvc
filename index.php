762f9ca875a4df76d8b8f68ee564a7f94c792bf0
antaww <anta.pizzetta@gmail.com>
2022-11-07 12:17:58 +0100

    feat: priority + php post

--- index.php
+++ index.php
@@ -14,24 +14,47 @@
                 <h1>Todo List</h1>
                 <hr/>
             </div>
-            <div class="input">
-                <input type="text" class="todotaker" id="inp"/>
-                <button class="todobtn" id="todobutton">
-                    <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="30"
-                            height="30"
-                            fill="#fff"
-                            class="bi bi-plus"
-                            viewBox="0 0 16 16"
-                    >
-                        <path
-                                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
-                        />
-                    </svg>
-                </button>
+            <form action="/" method="post">
+                <div class="input">
+                    <input type="text" class="todotaker" id="inp" placeholder="task" name="task" required/>
+                    <select class="priority" name="priority" required>
+                        <option value="">Priorité</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                    </select>
+                    <button type="submit" class="todobtn" id="todobutton">
+                        <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="30"
+                                height="30"
+                                fill="#fff"
+                                class="bi bi-plus"
+                                viewBox="0 0 16 16"
+                        >
+                            <path
+                                    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </form>
+            <div class="list">
+                <?php
+                ini_set('display_errors', 1);
+
+                global $pdo;
+                require('pdo.php');
+
+                if (isset($_POST['task'])) {
+                    $task = $_POST['task'];
+                    $priority = $_POST['priority'];
+
+                    $query = $pdo->prepare('INSERT INTO items (title, priority) VALUES (:title, :priority)');
+                    $query->execute(['title' => $task, 'priority' => $priority]);
+                }
+                ?>
             </div>
-            <div class="list"></div>
             <div class="clearall">
                 <button class="all">Clear All</button>
             </div>
@@ -41,4 +64,3 @@
         </div>
     </body>
 </html>
-<?php
