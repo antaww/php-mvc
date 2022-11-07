6c18b2e51c4ecae24748999dca041b41e141bc2a
antaww <anta.pizzetta@gmail.com>
2022-11-07 13:12:22 +0100

    fix: add task controller

--- src/controllers/add_task.php
+++ src/controllers/add_task.php
@@ -1,25 +1,18 @@
 <?php
 namespace App\Controllers\Tasks\Add;
 
-
 use Application\Model\Todo\Task;
 use Application\Model\Todo\TaskRepository;
 
 class AddTask
 {
-    function execute(array $input)
+    function execute(array $input): void
     {
-        ini_set('display_errors', 1);
-
-        global $database;
-        require('src/lib/database.php');
-
         if (isset($input['task']) && isset($input['priority'])) {
             $task = $input['task'];
             $priority = $input['priority'];
             $task_repository = new TaskRepository();
             $task_repository->addTask(new Task($task, $priority));
         }
-        return $database;
     }
 }
