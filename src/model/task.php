bb1251f0cd2e1bae25a9f867e95d0285ebf8b256
Bahsiik <o.mist1303@gmail.com>
2022-11-07 12:55:12 +0100

    fix: fix task repository

--- src/model/task.php
+++ src/model/task.php
@@ -13,18 +13,26 @@ class Task
 
 class TaskRepository
 {
+    public PDO $databaseConnection;
+
+    /**
+     * @param DatabaseConnection $databaseConnection
+     */
+    public function __construct(DatabaseConnection $databaseConnection)
+    {
+        $this->databaseConnection = $databaseConnection->getConnection();
+    }
+
     public function getTasks(): array
     {
-        $database = (new DatabaseConnection())->getConnection();
-        $statement = $database->prepare('SELECT * FROM tasks');
+        $statement = $this->databaseConnection->prepare('SELECT * FROM tasks');
         $statement->execute();
         return $statement->fetchAll(PDO::FETCH_CLASS, Task::class);
     }
 
     public function getTask(string $name): Task
     {
-        $database = (new DatabaseConnection())->getConnection();
-        $statement = $database->prepare('SELECT * FROM tasks WHERE name = :name');
+        $statement = $this->databaseConnection->prepare('SELECT * FROM tasks WHERE name = :name');
         $statement->execute(['name' => $name]);
         return $statement->fetchObject(Task::class);
     }
@@ -32,8 +40,7 @@ class TaskRepository
 
     public function addTask(Task $task): void
     {
-        $database = (new DatabaseConnection())->getConnection();
-        $statement = $database->prepare('INSERT INTO tasks (name, priority) VALUES (:name, :priority)');
+        $statement = $this->databaseConnection->prepare('INSERT INTO tasks (name, priority) VALUES (:name, :priority)');
         $statement->execute([
             'name' => $task->name,
             'priority' => $task->priority
@@ -42,22 +49,19 @@ class TaskRepository
 
     public function deleteTask(string $name): void
     {
-        $database = (new DatabaseConnection())->getConnection();
-        $statement = $database->prepare('DELETE FROM tasks WHERE name = :name');
+        $statement = $this->databaseConnection->prepare('DELETE FROM tasks WHERE name = :name');
         $statement->execute(['name' => $name]);
     }
 
     public function deleteAllTasks(): void
     {
-        $database = (new DatabaseConnection())->getConnection();
-        $statement = $database->prepare('DELETE FROM tasks');
+        $statement = $this->databaseConnection->prepare('DELETE FROM tasks');
         $statement->execute();
     }
 
     public function updatePriority(string $name, int $priority): void
     {
-        $database = (new DatabaseConnection())->getConnection();
-        $statement = $database->prepare('UPDATE tasks SET priority = :priority WHERE name = :name');
+        $statement = $this->databaseConnection->prepare('UPDATE tasks SET priority = :priority WHERE name = :name');
         $statement->execute([
             'name' => $name,
             'priority' => $priority
