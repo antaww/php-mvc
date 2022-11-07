931567f933e5398a5d3b8dc11eeeec5a560ed3f0
Ayfri <pierre.ayfri@gmail.com>
2022-11-07 14:40:08 +0100

    fix: Add missing action for adding tasks form.

--- templates/homepage.php
+++ templates/homepage.php
@@ -4,27 +4,27 @@
         <meta charset="UTF-8">
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="style.css">
-        <title>Document</title>
+	    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	    <link rel="stylesheet" href="style.css">
+	    <title>Document</title>
     </head>
-    <body>
-        <div class="container">
-            <div class="heading">
-                <h1>Todo List</h1>
-                <hr/>
-            </div>
-            <form action="/" method="post">
-                <div class="input">
-                    <input type="text" class="todotaker" id="inp" placeholder="task" name="task" required/>
-                    <select class="priority" name="priority" required>
-                        <option value="">Priorité</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                    </select>
-                    <button type="submit" class="todobtn" id="todobutton">
-                        <svg
+	<body>
+		<div class="container">
+			<div class="heading">
+				<h1>Todo List</h1>
+				<hr/>
+			</div>
+			<form action="/index.php?add" method="post">
+				<div class="input">
+					<input type="text" class="todotaker" id="inp" placeholder="task" name="task" required/>
+					<select class="priority" name="priority" required>
+						<option value="">Priorité</option>
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+					</select>
+					<button type="submit" class="todobtn" id="todobutton">
+						<svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="30"
                                 height="30"
