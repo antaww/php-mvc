ff48864a58326b50cf88b2e0244dc57e65153f79
Bahsiik <o.mist1303@gmail.com>
2022-11-07 12:14:42 +0100

    feat: add model todo

--- src/model/todo.php
+++ src/model/todo.php
@@ -4,14 +4,6 @@ namespace Application\Model\Todo;
 
 class Todo
 {
-    private $id;
-    private $todo;
-    private $status;
-
-    public function __construct($id, $todo, $status)
-    {
-        $this->id = $id;
-        $this->todo = $todo;
-        $this->status = $status;
-    }
+    public int $priority;
+    public string $name;
 }
